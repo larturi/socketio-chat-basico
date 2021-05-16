c937f7e3ce45b9faab7a738c254fea8e63f4ce5f
lea.arturi <lea.arturi@gmail.com>
2021-05-16 11:46:30 -0300

    CORS

--- models/server.js
+++ models/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const http = require('http');
 const socketio = require('socket.io');
 const path = require('path');
+const cors = require('cors');
 
 const Sockets = require('./sockets');
 
@@ -37,6 +38,9 @@ class Server {
     middlewares() {
         // Desplegar el directorio publico
         this.app.use( express.static( path.resolve(__dirname, '../public')) );
+
+        // Habilitar CORS
+        this.app.use(cors());
     }
 
     configurarSockets() {
